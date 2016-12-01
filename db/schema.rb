b8170c570a8222863ffac67af221f133b9e152dd
mikeotis123 <michael.otis2006@gmail.com>
2016-12-01 10:02:02 -0600

    category model and assigned plays to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201150107) do
+ActiveRecord::Schema.define(version: 20161201155204) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string   "title"
